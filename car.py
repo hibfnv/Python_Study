8600bc9bf12e30acaceebebcc513a7f520f6ade8
Eason Tsui <hibfnv@hotmail.com>
2020-08-22 11:37:24 +0800

    Add files via upload

--- car.py
+++ car.py
@@ -11,7 +11,25 @@ class Car():
         return car_name
 
     def get_odometer(self):
-        print("This car has " + str(self.odometer_reading) + " miles on it."
+        print("This car has " + str(self.odometer_reading) + " miles on it.")
+
+    def update_odometer(self, mileage):
+        """将里程设定为指定数值，禁止回滚"""
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("\033[31mRoll back odometer is forbidden!!\033[0m")
+
+    def increase_odometer(self, miles):
+        self.odometer_reading += miles
+
+
 my_car = Car('BMW', '525Li', 2019)
 print(my_car.get_description())
+my_car.get_odometer()
+print("When the car has run for 35780 miles:")
+my_car.update_odometer(35780)
+my_car.get_odometer()
+print("When the car has increase 300 miles odometer:")
+my_car.increase_odometer(300)
 my_car.get_odometer()
