c70c7b3b35aaee664b1dc140000702523d474a02
Eason Tsui <hibfnv@hotmail.com>
2020-08-30 21:46:36 +0800

    Add files via upload

--- rocket_function.py
+++ rocket_function.py
@@ -5,6 +5,32 @@ import sys
 import pygame
 
 
+# 第十节，将检测追踪键盘鼠标动作的内容再细分为两个按下和松开键盘部分
+def check_keydown_events(event, rocket):
+    """按下键盘获得的状态"""
+    if event.key == pygame.K_RIGHT:
+        rocket.move_right = True
+    elif event.key == pygame.K_LEFT:
+        rocket.move_left = True
+    elif event.key == pygame.K_UP:
+        rocket.move_up = True
+    elif event.key == pygame.K_DOWN:
+        rocket.move_down = True
+
+
+# 此处是松开键盘按键的状态
+def check_keyup_events(event, rocket):
+    """松开键盘按键后的状态呈现"""
+    if event.key == pygame.K_RIGHT:
+        rocket.move_right = False
+    elif event.key == pygame.K_LEFT:
+        rocket.move_left = False
+    elif event.key == pygame.K_UP:
+        rocket.move_up = False
+    elif event.key == pygame.K_DOWN:
+        rocket.move_down = False
+
+
 # 判断用户键盘和鼠标事件
 # 第七节，由于早期追踪键鼠没有带入参数，现在需要判断火箭的移动，所以要将rocket代入check_events(rocket)
 def check_events(rocket):
@@ -14,6 +40,8 @@ def check_events(rocket):
             sys.exit()
         # 这里通过对键盘右键的按下和松开，判断移动状态
         elif event.type == pygame.KEYDOWN:
+            """
+            此处内容通过新方法check_keydown_events(event, rocket)来完成
             if event.key == pygame.K_RIGHT:
                 rocket.move_right = True
             elif event.key == pygame.K_LEFT:
@@ -22,7 +50,11 @@ def check_events(rocket):
                 rocket.move_up = True
             elif event.key == pygame.K_DOWN:
                 rocket.move_down = True
+            """
+            check_keydown_events(event, rocket)
         elif event.type == pygame.KEYUP:
+            """
+            此处由新生成的方法取代，只需要直接调用check_keyup_events(event, rocket)来实现
             if event.key == pygame.K_RIGHT:
                 rocket.move_right = False
             elif event.key == pygame.K_LEFT:
@@ -31,6 +63,8 @@ def check_events(rocket):
                 rocket.move_up = False
             elif event.key == pygame.K_DOWN:
                 rocket.move_down = False
+            """
+            check_keyup_events(event, rocket)
 
 
 # 这里将主程序下面绘制屏幕的内容重新定义为update_screen方法
